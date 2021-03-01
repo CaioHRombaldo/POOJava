3203b141268ba46b2a8d4d35d26471e3721ce615
Caio Henrique Rombaldo <chrstream@hotmail.com>
2021-03-01 19:35:53 -0300

    Add Class 01 "Hello, World!"
    The first Class.

---
+++ Class-01/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author CaioHRombaldo
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Print "Hello, World!" for luck
+        System.out.println("Hello, World!");
+    }
+    
+}
