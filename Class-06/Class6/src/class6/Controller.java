1c66d03771473685eaa40f29081a8fa21207e6e1
Caio Henrique Rombaldo <chrstream@hotmail.com>
2021-03-08 21:21:13 -0300

    Add Controller interface

---
+++ Class-06/Class6/src/class6/Controller.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package class6;
+
+/**
+ *
+ * @author CaioHRombaldo
+ */
+public interface Controller {
+    
+    // Here we declare all the interface methods of our class in an abstract way.
+    public abstract void turnOn();
+    
+}
